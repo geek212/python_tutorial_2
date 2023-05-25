929743bc81e243555b004247fd55fd224b753430
Ashwin nair <95960077+geek212@users.noreply.github.com>
2023-05-25 17:33:39 +0530

    Add files via upload

---
+++ oop.py
@@ -0,0 +1,43 @@
+# Sloving problem by creating objects is one of  the most popular approches programming
+# This called objects orinted programming  
+
+
+''' The term object stems from the fact that 
+the model resembles objects in the real word in that each object is a thing that has 
+certain attributes and characteristics that make it unique '''
+
+
+# Example: 1
+
+''' We all know cars right diffrent model , year, etc When we  see as one . 
+But its not it as diffrent attributes that make unique , They have certain methods in common, where a method is an action or a thing the car can do. For example, cars all 
+have go, stop, and turn actions you can control pretty much the same in each one of cars'''
+
+
+# Cars class 
+
+# Attributes  : methods
+#   .year        .go()
+#   .make        .stop()
+#   .model       .turn()
+#   .colors      .heat()
+#   .doors       .park()
+
+
+
+# Even people can easily be viewed as objects in this manner. In your code, you 
+# could create a Member class with which you manage your site members. Each 
+# member would have certain attributes, such as a username, full name, and so 
+# forth. You could also have methods such as .archive() to deactivate an account 
+# and .restore() to reactivate an account. The .archive() and .restore()
+
+
+# methods are behaviors that let you control membership, in much the same way the 
+# accelerator, brake, and steering wheel allow you to control a car. 
+
+
+
+# This business of classes and instances stems from a type of programming 
+# called object-oriented programming (OOP for short). It’s been a major concept 
+# in programming the biz for a few decades now. Python, like any significant, serious, modern 
+#  language is object-oriented.
